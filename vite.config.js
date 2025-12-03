65581e2d9c0702532617a38de4e16169a7a3b3d5
Yupei Cai <caiyp7@outlook.com>
2025-12-03 22:12:00 +0800

    Fix resource paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/yupei-cai-website/' : '/',
+  base: process.env.VITE_BASE || (process.env.NODE_ENV === 'production' ? '/yupei-cai-website/' : '/'),
 })
 
